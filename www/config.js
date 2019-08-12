6a7407438bbed6cf2fb313cfe5a0d91aeab6f30a
n8tb1t <n8tb1t@gmail.com>
2019-08-12 15:30:44 +0500

    add www

---
+++ www/config.js
@@ -0,0 +1,18 @@
+var api = "https://pool.cx-pool.gq/cat";
+var apiMerged = "https://pool.cx-pool.gq/xun";
+
+var email = "n8tb1t@gmail.com";
+var telegram = "";
+var discord = "https://discord.gg/byKYDyb";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var blockchainExplorer = "https://www.cx-explorer.gq/index.html?hash={id}#blockchain_block";
+var transactionExplorer = "https://www.cx-explorer.gq/index.html?hash={id}#blockchain_transaction";
+
+
+var blockchainExplorerMerged = "http://explorer.ultranote.org/?hash={id}#blockchain_block";
+var transactionExplorerMerged = "http://explorer.ultranote.org/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
